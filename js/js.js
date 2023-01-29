98dbb5556ec21026b8357dcee2e638e4731a837d
Nastassia Derugo <anastasia.derugo@gmail.com>
2023-01-29 21:13:12 +0300

    bug fix js

--- js/js.js
+++ js/js.js
@@ -22,13 +22,3 @@ function alphabetPosition(text) {
 }
 alphabetPosition("Hello world");
 
-function alphabetMax(max, min){
-    var listOfItems = 0;
-    describe("Test", function(){
-        it("Examples", function(){
-          Test.assertEquals(min([-52, 56, 30, 29, -54, 0, -110]), -110);
-          Test.assertEquals(min([42, 54, 65, 87, 0]), 0);
-          Test.assertEquals(max([4,6,2,1,9,63,-134,566]), 566);
-          Test.assertEquals(max([5]), 5);
-        });
-}
