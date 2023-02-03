95896795bf97edfa0295f36a376289ba3d265b41
Nastassia Derugo <anastasia.derugo@gmail.com>
2023-02-03 22:32:57 +0300

    alternative js

--- js/js.js
+++ js/js.js
@@ -21,4 +21,15 @@ function alphabetPosition(text) {
 		}
 	}).join(" ");
 }
-alphabetPosition("Hello world");*/
+alphabetPosition("Hello world");
+String.prototype.toAlternatingCase = function () {
+	let str = '';
+	 for(let i = 0; i< this.length; i++){
+	   if (this[i] == this[i].toLowerCase()){ 
+		 str += this[i].toUpperCase();
+	   } else {
+		 str += this[i].toLowerCase();
+	   }
+	 }
+	 return str;
+   }*/
