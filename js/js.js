7fbab7d2efe47d201d910c08a438cb7e187a02a1
Nastassia Derugo <anastasia.derugo@gmail.com>
2023-02-13 22:48:13 +0300

    stray numbers

--- js/js.js
+++ js/js.js
@@ -61,4 +61,13 @@ String.prototype.toAlternatingCase = function () {
 	  arr.push(number);
 	}
 	return arr;
+  }
+  //stray numbers
+  function stray(numbers) {
+	var a = numbers.sort();
+	
+	if(a[0] != a[1]) {
+	  return a[0]
+	} 
+	return a[a.length-1]
   }*/
